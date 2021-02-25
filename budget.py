0c704ff81bfde8891657550cd637bfce90384d9e
gtburchett <gburchett88@gmail.com>
2021-02-24 23:52:06 -0600

    Finished coding the create_spend_chart function. Still not working properly.
    Coded the title and bars to appear on the spend chart. The dashes and the stacking of the category names still needs to be worked on.

--- budget.py
+++ budget.py
@@ -2,6 +2,7 @@ class Category:
     def __init__(self, cat_name):
         self.cat_name = cat_name
         self.ledger = []
+        self.withdrawals = []
     
     def __str__(self):    
         formatted_ledger = []
@@ -22,6 +23,7 @@ class Category:
         self.withdraw_desc = withdraw_desc
         if self.check_funds(withdraw_amt):
             self.ledger.append({'amount': -1 * withdraw_amt, 'description': withdraw_desc})
+            self.withdrawals.append(withdraw_amt)
             return True
         else:
             return False
@@ -51,5 +53,60 @@ class Category:
         self.amount = amount
         return amount <= self.get_balance()
 
+def calculate_total_spent(categories):
+    total_spent = 0
+    for category in categories:
+        total_spent += sum(category.withdrawals)
+
+    return total_spent
+
+def percentage_spend_per_category(categories, total_spent):
+    category_spending = []
+    for category in categories:
+        category_spend = (sum(category.withdrawals) / total_spent) * 100
+        category_spending.append(category_spend)
+
+    return category_spending
+
+def stacked_category_names(categories):
+    formatted_categories = ""
+    len_of_longest_name = 0
+    for category in categories:
+        if len(category.cat_name) > len_of_longest_name:
+            len_of_longest_name = len(category.cat_name)
+    
+    count = 1
+    while count <= len_of_longest_name:
+        row = []
+        for i in range(len_of_longest_name):
+            row.append("     ")
+            for category in categories:
+                if i <= len(category.cat_name)-1:
+                    row.append(category.cat_name[i] + "  ")
+                else:
+                    row.append("   ")
+            row_of_letters = "".join(row)
+            formatted_categories += row_of_letters + "\n"
+            count += 1
+    
+    return formatted_categories
+
 def create_spend_chart(categories):
-    pass
+    dashes = '-' * 10
+    y_axis = 100
+    chart = "Percentage spent by category\n"
+
+    while y_axis > -1:
+        bar = []
+        for percent in percentage_spend_per_category(categories, calculate_total_spent(categories)):
+            if percent >= y_axis:
+                bar.append("o  ")
+            else:
+                bar.append("   ")
+        bars = "".join(bar)
+        chart += str(y_axis).rjust(3) + "| " + bars + "\n"
+        y_axis -= 10
+    
+    chart += "    " + dashes + "\n" + stacked_category_names(categories)
+
+    return chart
